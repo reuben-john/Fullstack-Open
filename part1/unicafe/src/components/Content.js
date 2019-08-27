dfb92723cc08319364f1c3b31eef0cd7c0cb5704
Reuben John <rjkoppy@gmail.com>
2019-08-27 10:20:28 -0500

    Seperate out Part component

--- part1/unicafe/src/components/Content.js
+++ part1/unicafe/src/components/Content.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Part from './Part';
+import Total from './Total';
 
-const Header = ({ part, exercise }) => {
+const Content = ({ part, exercise }) => {
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
   return (
-    <p>
-      {part} {exercise}
-    </p>
+    <div>
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
   );
 };
 
-export default Header;
+export default Content;
