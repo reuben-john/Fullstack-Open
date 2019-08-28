6866756ca2059ac1fec43516e83f6948865826da
Reuben John <rjkoppy@gmail.com>
2019-08-27 22:11:44 -0500

    Refactor app to use objects

--- part1/unicafe/src/components/Content.js
+++ part1/unicafe/src/components/Content.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import Part from './Part';
-import Total from './Total';
 
-const Content = ({ part, exercise }) => {
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
-
+const Content = ({ parts }) => {
   return (
     <div>
-      <Part part={part1} exercise={exercises1} />
-      <Part part={part2} exercise={exercises2} />
-      <Part part={part3} exercise={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Part part={parts[0].name} exercise={parts[0].exercises} />
+      <Part part={parts[1].name} exercise={parts[1].exercises} />
+      <Part part={parts[2].name} exercise={parts[2].exercises} />
     </div>
   );
 };
